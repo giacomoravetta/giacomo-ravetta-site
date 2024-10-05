e309ea16e9f88fb9a1a97d029642bec3a588df99
Giacomo Ravetta <102873147+giacomoravetta@users.noreply.github.com>
2024-10-05 11:52:50 +0200

    Complete redesign of the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import solidJs from '@astrojs/solid-js';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs(), tailwind(), react()]
 });
