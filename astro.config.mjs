12948e7b54e764fa2979bff0c49c2ddc81bb4a5b
Giacomo Ravetta <102873147+giacomoravetta@users.noreply.github.com>
2025-02-14 05:15:28 +0100

    Start of a new redesign! Created the initial structure and the Hero.svelte component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import solidJs from "@astrojs/solid-js";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
