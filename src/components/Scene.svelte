533baa30b09845902e4232e6d063e54c429f9d33
Giacomo Ravetta <102873147+giacomoravetta@users.noreply.github.com>
2025-03-04 21:28:07 +0100

    Added new files Svelte + Deno

---
+++ src/components/Scene.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { T } from "@threlte/core";
+</script>
+
+<T.PerspectiveCamera
+    makeDefault
+    position={[10, 10, 10]}
+    oncreate={(ref) => {
+        ref.lookAt(0, 1, 0);
+    }}
+/>
+
+<T.DirectionalLight position={[0, 10, 10]} castShadow />
+
+<T.Mesh position.y={1} castShadow>
+    <T.BoxGeometry args={[1, 2, 1]} />
+    <T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
+
+<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
+    <T.CircleGeometry args={[4, 40]} />
+    <T.MeshStandardMaterial color="white" />
+</T.Mesh>
