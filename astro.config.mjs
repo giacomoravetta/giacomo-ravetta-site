a254333b82e733aa9734388ee5b6b76c33fa05b6
Giacomo Ravetta <102873147+giacomoravetta@users.noreply.github.com>
2025-03-28 18:19:15 +0100

    Added extensions and integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import solidJs from "@astrojs/solid-js";
+import svelte from '@astrojs/svelte';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
