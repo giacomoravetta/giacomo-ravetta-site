d1757a732bc01c3b9c5cacf0885b90f26ee52b4a
Giacomo Ravetta <102873147+giacomoravetta@users.noreply.github.com>
2025-04-09 23:57:42 +0200

    feat: new website!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import solidJs from "@astrojs/solid-js";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
